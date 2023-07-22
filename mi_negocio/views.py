329105ff091cefe166863ce9f182d46dc02a714f
Mauricio Aguilera <mauricio@familify.com>
2023-07-22 09:43:30 -0500

    create template views

---
+++ mi_negocio/views.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+DEBUG = settings.DEBUG
+TOKEN_AUTHENTICATION = settings.TOKEN_AUTHENTICATION
+
+
+class BaseViewSet(viewsets.ViewSet):
+    """
+    Vista basica sin permisos
+    """
+
+
+class IsAuthViewSet(BaseViewSet):
+    """
+    Vista con permisos solo para usuarios autenticados
+    """
+    if TOKEN_AUTHENTICATION:
+        permission_classes = [IsAuthenticated,]
+
+
+class IsAdminViewSet(BaseViewSet):
+    """
+    Vista con permisos solo para superusuarios
+    """
+    if TOKEN_AUTHENTICATION:
+        permission_classes = [IsAuthenticated, IsAdminUser,]
