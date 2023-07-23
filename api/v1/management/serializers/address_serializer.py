a659ddb53048f64ee0067da315b01c3349e125d7
Mauricio Aguilera <mauricio@familify.com>
2023-07-22 19:06:26 -0500

    validate only one matriz address

--- api/v1/management/serializers/address_serializer.py
+++ api/v1/management/serializers/address_serializer.py
@@ -17,8 +17,20 @@ class AddressSerializer(ModelSerializer):
         )
 
     def validate(self, data):
-        client = data.get('client')
-        if data.get('is_matriz') and Address.objects.filter(client=client).exists():
+        instance = self.instance
+        data_client = data.get('client')
+        if instance:
+            if data_client and instance.client.id != data_client:
+                client_id = data.get('client')
+            else:
+                client_id = instance.client.id
+            instance_id = self.instance.id
+        else:
+            instance_id = None
+            client_id = data.get('client')
+        if data.get('is_matriz') and \
+            Address.objects.filter(client=client_id,
+                                   is_matriz=True).exclude(id=instance_id).exists():
             raise ValidationError(
                 {'is_matriz': 'Ya existe una address matriz vinculada a este cliente.'})
         return data
