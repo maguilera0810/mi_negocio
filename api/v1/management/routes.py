a805ba09dbb13e66e9bcdc8ecc7fc993c37c9470
Mauricio Aguilera <mauricio@familify.com>
2023-07-22 19:05:04 -0500

    add address to routes

--- api/v1/management/routes.py
+++ api/v1/management/routes.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
 from api.v1.management.handlers.client_handler import ClientHandler
+from api.v1.management.handlers.address_handler import AddressHandler
 
 urlpatterns = [
     path('client/', ClientHandler.as_view({'get': 'list',
-                                             'post': 'create'}), name='client'),
+                                           'post': 'create'}), name='client'),
     path('client/<int:id>', ClientHandler.as_view({'get': 'retrieve',
-                                                      'put': 'update',
-                                                      'delete': 'delete'}), name='client_id'),
+                                                   'put': 'update',
+                                                   'delete': 'delete'}), name='client_id'),
+]
+urlpatterns += [
+    path('address/', AddressHandler.as_view({'get': 'list',
+                                             'post': 'create'}), name='address'),
+    path('address/<int:id>', AddressHandler.as_view({'get': 'retrieve',
+                                                     'put': 'update',
+                                                     'delete': 'delete'}), name='address_id'),
 ]
